a44c6a837897e5e3e2b8172fa00577b4c8347038
daiyadeguchi <double.d.rugby@gmail.com>
2025-08-22 06:51:16 +0900

    not working, but can add sad movies to the db

--- app/services/chatbot_service.rb
+++ app/services/chatbot_service.rb
@@ -9,29 +9,30 @@ class ChatbotService
     req["Content-Type"] = "application/json"
     req["Authorization"] = "Bearer justatest"
 
-    movie_to_hasharray(get_movies(http, req, "cry"), :sad)
-    # hash.push(movie_to_hasharray(get_movies(http, req, "laugh")))
-
-    # movie_to_hasharray(JSON.parse(movies))
+    hash = movie_to_hasharray(get_movies(http, req, "cry"), :sad)
+    hash.push(movie_to_hasharray(get_movies(http, req, "laugh"), :laugh))
   end
 
   private
+
+  @@models = { jan_v1: "Jan-v1-4B-Q4_K_M", jan_nano: "jan-nano-128k-Q4_K_M" }
 
   def get_movies(http, req, category_verb)
     req.body = construct_body(category_verb)
     res = http.request(req)
     if res.is_a?(Net::HTTPSuccess)
       res_body = JSON.parse(res.body)
-      res_body["choices"][0]["message"]["content"].split("</think>")[1]
+      lines = res_body["choices"][0]["message"]["content"].split("\n")
+      lines.inspect # inspect to make array to string for future JSON.parse
     else
       puts "Error getting movies from jan: #{res.code}"
     end
   end
 
   def construct_body(category)
-    content = "Give me a json formatted list of five movies that makes me #{category}. for example { 'title': 'ショーシャンクの空に' }. Please make it very random as well."
+    content = "Give me a json formatted list of 15 movies that makes me #{category}. for example { 'title': 'ショーシャンクの空に' }. You don't need to add markdown such as ```json. Please make it very random as well."
     data = {
-      "model": "Jan-v1-4B-Q4_K_M",
+      "model": @@models[:jan_nano],
       "messages": [ { "role": "user", "content": content } ]
     }
     data.to_json
@@ -41,6 +42,7 @@ class ChatbotService
     movies = JSON.parse(movies)
     movies_hasharray = []
     movies.each do |movie|
+      movie = JSON.parse(movie)
       movie_hash = { hashed_id: movie["title"].hash, title: movie["title"], category: category }
       movies_hasharray.push(movie_hash)
     end
--- lib/tasks/call_movie.rake
+++ lib/tasks/call_movie.rake
@@ -1,10 +1,12 @@
+categories = { sad: 1, laugh: 2 }
+
 namespace :call_movie do
   desc "testing rake"
   task call: :environment do
     movie = Movie.new
     movies = movie.create_movies
     movies.each do |movie|
-      Movie.create(hashed_id: movie[:hashed_id], title: movie[:title], category: movie[:category])
+      Movie.create(hashed_id: movie[:hashed_id], title: movie[:title], category: categories[movie[:category]])
     end
   end
 end
